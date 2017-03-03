e0f01ff7822bffdc63d9c0c181e20f3015b2b208
Akvile <kiskisakvile@gmail.com>
2017-03-02 20:44:43 -0600

    minor final fixes that linter messed with

--- js/site.js
+++ js/site.js
@@ -7,11 +7,12 @@ jQuery(function ($) {
   $("#email").on("keyup focus blur", function () {
     var currentValue = $(this).val();
     var validPattern = $(this).attr("pattern");
-    console.log(currentValue, validPattern);
     if (currentValue.match(validPattern)) {
       $("#submit").addClass("active");
     } else {
       $("#submit").removeClass("active");
     }
+
   });
 });
+ 
