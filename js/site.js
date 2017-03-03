15b4266e31523c27aaa40d36291324c04dff1328
Akvile <kiskisakvile@gmail.com>
2017-03-02 21:58:42 -0600

    added in javacript for emptying email field and keeping url the same upon submission. Also modified email input field so @ sign wasn't cut off

--- js/site.js
+++ js/site.js
@@ -4,6 +4,7 @@ jQuery(function ($) {
   $("html").removeClass("nojs");
   $("html").addClass("hasjs");
 
+  // check for valid email 
   $("#email").on("keyup focus blur", function () {
     var currentValue = $(this).val();
     var validPattern = $(this).attr("pattern");
@@ -12,7 +13,16 @@ jQuery(function ($) {
     } else {
       $("#submit").removeClass("active");
     }
-
+  
+  // empty out email field and refresh page without modifying url upon click
+  $("#submit").click(function (event) {
+    event.preventDefault();
+    $("#email").val("");
+    window.location.reload();
+    
   });
+
+});
+
 });
  
